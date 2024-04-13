d0b925eb3565512c684f8267970763e177ce4d86
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-13 10:54:40 +0000

    container config done

---
+++ 01_basics/3.datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  // treat all JS code as newer version
+
+// alert ( 3 + 3)  // We are using nodejs, not browser so reult will not come as 6
+
+
+console.log(3 
+    + 3)        //code readability should be high
+
+
+    let name = "Jit"                     // String
+    let age = 18                        //NUMBER
+    let isLoggedIn = false              // Boolean
+    let abc = ["jit","sunny","tukai"]   //Array or object
+    let state                           //Undefined
+    let password = null                 //null
+                                       //Symbol => unique
+
+
+// Object
+
+console.log(typeof(undefined))      // undefined
+console.log(typeof(null))           // object
