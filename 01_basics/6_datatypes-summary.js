46025c52d18ac5c35abce4e45ee314beeef6847b
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-13 12:59:43 +0000

    summary

---
+++ 01_basics/6_datatypes-summary.js
@@ -0,0 +1,8 @@
+// Primitive Data Types (Call by Value)
+
+// 7 types : String, Number , Boolean , Null , undefined , Symbol , BigInt
+
+
+// Non Primitive (Call by Reference)
+
+// 3 types : Array , Objects , Fumctions
