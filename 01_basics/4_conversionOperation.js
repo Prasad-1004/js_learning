36ecd28976711e598781abbd1706f85b4a05e629
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-13 11:21:16 +0000

    Conversion

---
+++ 01_basics/4_conversionOperation.js
@@ -0,0 +1,58 @@
+let score = "33";
+
+console.log(typeof(score));                //String
+
+score = "33abc"
+
+let valueInNumber = Number(score);         
+console.log(typeof(valueInNumber));        // number
+console.log(valueInNumber);                // NaN
+
+
+
+/*
+let score = "null";
+
+console.log(typeof(score));                //object
+
+
+
+let valueInNumber = Number(score);         
+console.log(typeof(valueInNumber));        // number
+console.log(valueInNumber);                // 0
+
+
+
+
+
+
+let score = undefined;
+
+console.log(typeof(score));                //undefined
+
+
+
+let valueInNumber = Number(score);         
+console.log(typeof(valueInNumber));        // number
+console.log(valueInNumber);                // NaN
+
+
+
+
+
+
+
+
+
+let score = true;
+
+console.log(typeof(score));                //boolean
+
+
+
+let valueInNumber = Number(score);         
+console.log(typeof(valueInNumber));        // number
+console.log(valueInNumber);                // 1
+
+
+*/
