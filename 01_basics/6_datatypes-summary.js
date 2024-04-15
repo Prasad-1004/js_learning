db6e7d348889fa0e70ca6164664451056f929cf2
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-15 12:14:18 +0000

    summary

--- 01_basics/6_datatypes-summary.js
+++ 01_basics/6_datatypes-summary.js
@@ -2,7 +2,28 @@
 
 // 7 types : String, Number , Boolean , Null , undefined , Symbol , BigInt
 
+const score = 100                         //numbers
+const scoreValue = 100.3                  //numbers
+
+const isLoggedIn = false                  //boolean
+const temp = null                         //null
+let userName                              //undefined
+
+const id = Symbol('123')                  
+const id2 = Symbol('123')                 //symbol  (The reult will be different)
+
 
 // Non Primitive (Call by Reference)
 
 // 3 types : Array , Objects , Fumctions
+
+const heros = ["IronMan", "batman", "Flash"]            // Array
+
+let myObj = {
+    name: "Jit",
+    age: 25 
+}                                                       //Object
+
+const myFunction = function(){
+    console.log("Hello Jit");
+}                                                       //function
