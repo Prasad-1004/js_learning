35868763941cccbbdb895f55ac3f18c6cd40db46
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-18 14:16:35 +0000

    array

--- 02_basics/1_array.js
+++ 02_basics/1_array.js
@@ -38,3 +38,17 @@ console.log(typeof newArr)
 
 //slice , spice
 
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1)
+
+
+
+console.log("B", myArr)
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr)
+console.log(myn2)
+
+
