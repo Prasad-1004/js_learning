64d3da44aecb963dffd3f5318da26f82c5423c2c
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-18 16:40:09 +0000

    objects

---
+++ 02_basics/4_objects.js
@@ -0,0 +1,11 @@
+const tinderUser = new Object()           // This is a singleton object
+
+const tinder = {}                      // This is not a singleton object
+
+tinder.id = "123abc"
+tinder.name = "jit"
+tinder.isLoggedIn = false
+
+console.log(tinder)
+
+
