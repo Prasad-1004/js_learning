ee6460b5470121fb123033dc629ee70b2a53c034
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-18 08:17:15 +0000

    array

---
+++ 02_basics/1_array.js
@@ -0,0 +1,40 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["Ironman", "Flash"]
+
+
+const myArr2 = new Array(1,2,3,4,5)
+
+
+console.log(myArr[0])
+
+
+// Array Methods
+
+myArr.push(6)
+myArr.push(7)
+console.log(myArr)
+
+myArr.pop()
+console.log(myArr)
+
+
+myArr.unshift(9)
+console.log(myArr)
+
+myArr.shift()
+console.log(myArr)
+
+
+console.log(myArr.includes(9))
+console.log(myArr.indexOf(9)) 
+
+
+const newArr = myArr.join()
+
+console.log(typeof(myArr))
+console.log(typeof newArr)
+
+
+
+//slice , spice
+
