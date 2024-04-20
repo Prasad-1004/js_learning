d64d322514334bad407a2f1bce2486b61d56214f
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-04-20 08:09:50 +0000

    functions

---
+++ 03_basics/1_functions.js
@@ -0,0 +1,14 @@
+
+
+function say() {
+    console.log("H")
+    console.log("I")
+    console.log("")
+    console.log("J")
+    console.log("I")
+    console.log("T")
+}
+
+say       //---->  reference
+
+say()    //------> Execution
